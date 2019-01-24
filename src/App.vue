b9f5c9a2b98ce374cdf275888902c3322e16a69f
sciuridae0603 <sciuridae0603@gmail.com>
2019-01-24 16:36:20 +0800

    fix css

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,13 @@ export default {
   }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC');
+#app {
+  font-family: 'Noto Sans TC', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>

aab1003fb4c147ab1b24201d60e97e213796331c
sciuridae0603 <sciuridae0603@gmail.com>
2019-01-24 16:31:26 +0800

    add basic files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+
+  </div>  
+</template>
+
+<script>
+import HTMLMetadataGenerator from './components/HTMLMetadataGenerator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HTMLMetadataGenerator
+  }
+}
+</script>
